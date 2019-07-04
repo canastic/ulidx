7206bc47534c2ef5d19801e5a83dcfe1c46f8718
Toni Cárdenas <toni@tcardenas.me>
2019-07-04 10:29:44 +0200

    Make thread-safe.

--- ulidx.go
+++ ulidx.go
@@ -2,12 +2,16 @@ package ulidx
 
 import (
 	"math/rand"
+	"sync"
 
 	"github.com/oklog/ulid"
 )
 
 var entropy = ulid.Monotonic(rand.New(rand.NewSource(int64(ulid.Now()))), 0)
+var mtx sync.Mutex
 
 func New() string {
+	mtx.Lock()
+	defer mtx.Unlock()
 	return ulid.MustNew(ulid.Now(), entropy).String()
 }
